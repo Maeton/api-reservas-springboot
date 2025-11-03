46a600af273c203f3fddbc43d7adae3ec6a07b9c
Mateoeo <Mateo2nogueira@gmail.com>
2025-11-03 16:02:34 -0300

    Docs: Agrega README.md y limpieza final de entidades

--- src/main/java/cl/ProyectoFinalReserva/Reserva.java
+++ src/main/java/cl/ProyectoFinalReserva/Reserva.java
@@ -17,12 +17,12 @@ public class Reserva {
     @JoinColumn(name = "servicio_id_servicio")
     private Servicio servicio;
 
-    // --- 1. REQUERIDO POR JPA ---
+
     public Reserva() {
         // Constructor vacío
     }
 
-    // --- 2. FALTABAN ESTOS ---
+
     public Long getId() {
         return id;
     }
@@ -31,9 +31,6 @@ public class Reserva {
         this.id = id;
     }
 
-    // --- TUS MÉTODOS (Limpiados) ---
-    // (No es necesario el "cl.ProyectoFinalReserva."
-    // pero funciona de ambas formas)
 
     public Servicio getServicio() {
         return servicio;
